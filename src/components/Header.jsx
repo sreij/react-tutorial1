d0ecb592bf9f28e3018fddd254cb46ac6d636e0d
sreij <chrj23020@g.nihon-u.ac.jp>
2024-10-07 11:24:15 +0900

    Navigation.jsxの追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import Navi from "./Navigation";
+
 export default function Header() {
     return (
         <header className="hero is-dark is-bold">
@@ -5,15 +7,9 @@ export default function Header() {
                 <div className="container">
                     <h1 className="title">BIRDWATCHING</h1>
                     <img src="dove.png" alt="a simple dove logo" />
-                    
-                    <ul>
-                        <li><span>Home</span></li>
-                        <li><a href="#">Get started</a></li>
-                        <li><a href="#">Photos</a></li>
-                        <li><a href="#">Gear</a></li>
-                        <li><a href="#">Forum</a></li>
-                    </ul>
+                    <Navi />
                 </div>
+                
             </div>
         </header>
     );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,9 +10,10 @@ export default function Main() {
                     <p>Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas, tips, and photos with other like-minded people.</p>
 
                     <p>So don't waste time! Get what you need, then turn off that computer and get out into the great outdoors!</p>
-
-                    <Gallery />
                 </div>
+            </section>
+            <section className="section">
+                <Gallery />
             </section>
         </main>
     );
