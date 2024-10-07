70fa7113136b77f99c940ce81933a834a256f063
sreij <chrj23020@g.nihon-u.ac.jp>
2024-10-07 17:41:13 +0900

    section移動

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,8 +4,8 @@ export default function Gallery() {
   return (
     <div className="columns is-vcentered is-multiline">
       <div className="column is-3">
-        <h2>Favourite photos</h2>
         <section>
+          <h2>Favourite photos</h2>
           <Image />
         </section>
       </div>
