1fcb655a164ff9cd35483f9b9c585cd99371db92
sreij <chrj23020@g.nihon-u.ac.jp>
2024-10-08 10:15:46 +0900

    Article.jsxの追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,12 @@
+import Article from "./Article"
 import Gallery from "./Gallery";
 
 export default function Main() {
     return (
         <main>
-            <section>
-                <article>
-                    <div className="container">
-                        <h2>Welcome</h2>
-
-                        <p>
-                            Welcome to our fake birdwatching site.
-                            If this were a real site, it would be the ideal place to come to learn more about birdwatching,
-                            whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas,
-                            tips, and photos with other like-minded people.
-                        </p>
-
-                        <p>
-                            So don't waste time! Get what you need,
-                            then turn off that computer and get out into the great outdoors!
-                        </p>
-
-                    </div>
-                </article>
-            </section>
+            <Article />
             <aside>
-                <div className="container">
-                    <Gallery />
-                </div>
+                <Gallery />
             </aside>
         </main>
     );
